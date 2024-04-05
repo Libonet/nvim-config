9aa32158d36481a6cf9125e0b5241031b1a17447
Santiago Libonati <santiago.libonati@gmail.com>
2024-04-05 20:14:22 -0300

    agregado plugin lualine

--- init.lua
+++ init.lua
@@ -16,4 +16,7 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup("Libo.plugins")
 
+local builtin = require("telescope.builtin")
+vim.keymap.set('n', '<C-p>', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 
