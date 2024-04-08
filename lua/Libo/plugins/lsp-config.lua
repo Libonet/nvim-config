fd5e65336e562c9c448e7232caf9b47e153e89b3
Santiago Libonati <santiago.libonati@gmail.com>
2024-04-07 23:32:50 -0300

    agregado lsp de haskell

--- lua/Libo/plugins/lsp-config.lua
+++ lua/Libo/plugins/lsp-config.lua
@@ -19,11 +19,15 @@ return {
       local lspconfig = require("lspconfig")
       lspconfig.lua_ls.setup({})
       lspconfig.clangd.setup({})
-      lspconfig.
+      lspconfig.hls.setup({})
 
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
       vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
       vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
+
+      local bufopts = { noremap = true, silent = true, buffer = bufnr }
+      vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, bufopts)
+      vim.keymap.set('n', '<leader>r', vim.diagnostic.goto_next, {})
     end
   }
 }
