a0c9ccb1ebef83d5775afa909a0ad4898f22e0f7
Santiago Libonati <santiago.libonati@gmail.com>
2024-04-09 00:51:10 -0300

    changed live grep from <leader>fg to <leader>lg

--- lua/Libo/plugins/lsp-config.lua
+++ lua/Libo/plugins/lsp-config.lua
@@ -1,33 +1,33 @@
 return {
-  {
-    "williamboman/mason.nvim",
-    config = function()
-      require("mason").setup()
-    end
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    config = function()
-      require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "clangd" }
-      })
-    end
-    },
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      local lspconfig = require("lspconfig")
-      lspconfig.lua_ls.setup({})
-      lspconfig.clangd.setup({})
-      lspconfig.hls.setup({})
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = { "lua_ls", "clangd" },
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local lspconfig = require("lspconfig")
+			lspconfig.lua_ls.setup({})
+			lspconfig.clangd.setup({})
+			lspconfig.hls.setup({})
 
-      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-      vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
 
-      local bufopts = { noremap = true, silent = true, buffer = bufnr }
-      vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, bufopts)
-      vim.keymap.set('n', '<leader>r', vim.diagnostic.goto_next, {})
-    end
-  }
+			local bufopts = { noremap = true, silent = true, buffer = bufnr }
+			vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, bufopts)
+			vim.keymap.set("n", "<leader>r", vim.diagnostic.goto_next, {})
+		end,
+	},
 }
