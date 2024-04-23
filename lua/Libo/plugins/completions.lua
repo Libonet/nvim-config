a17ec7d7918009a43b4674bdb0241837d094df7d
Santiago Libonati <santiago.libonati@gmail.com>
2024-04-23 14:52:39 -0300

    added build step to luasnip
    It builds the jsregexp library. It should be installed already on the
    machine

--- lua/Libo/plugins/completions.lua
+++ lua/Libo/plugins/completions.lua
@@ -2,12 +2,16 @@ return {
 	{
 		"hrsh7th/cmp-nvim-lsp",
 	},
-	{
+  {
 		"L3MON4D3/LuaSnip",
 		dependencies = {
 			"saadparwaiz1/cmp_luasnip",
 			"rafamadriz/friendly-snippets",
 		},
+      -- follow latest release.
+      version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+      -- install jsregexp (optional!).
+      build = "make install_jsregexp"
 	},
 	{
 		"hrsh7th/nvim-cmp",
