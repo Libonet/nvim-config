f6cfbfdec949823bfeb21421af2ec42460c260af
Santiago Libonati <santiago.libonati@gmail.com>
2024-05-10 01:49:51 -0300

    Remaps varios

--- lua/Libo/plugins/neo-tree.lua
+++ lua/Libo/plugins/neo-tree.lua
@@ -8,7 +8,7 @@ return {
     -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
   },
   config = function()
-    vim.keymap.set("n", "<C-n>", ':Neotree filesystem reveal left toggle<CR>')
+--    vim.keymap.set("n", "<C-n>", ':Neotree filesystem reveal left toggle<CR>')
   end
 }
 
--- lua/Libo/plugins/telescope.lua
+++ lua/Libo/plugins/telescope.lua
@@ -16,6 +16,7 @@ return {
 			vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
 			vim.keymap.set("n", "<leader>lg", builtin.live_grep, {})
 			vim.keymap.set("n", "<leader>ob", builtin.buffers, {})
+			vim.keymap.set("n", "<leader>lf", builtin.current_buffer_fuzzy_find, {})
 		end,
 	},
 	{
