a3aabc75c7c6823824b7ec2afc9753f80790075d
Santiago Libonati <santiago.libonati@gmail.com>
2024-05-10 01:48:27 -0300

    Agregada busqueda de projecto

--- lua/Libo/plugins/alpha.lua
+++ lua/Libo/plugins/alpha.lua
@@ -22,7 +22,8 @@ return {
     -- Set menu
     dashboard.section.buttons.val = {
       dashboard.button("e", "  > New file", ":ene <BAR> startinsert <CR>"),
-      dashboard.button("f", "󰈞  > Find file", "<cmd>lua require'telescope.builtin'.find_files({ find_command = {'rg', '--files', '--hidden', '--iglob', '!.git' }})<CR>"),
+      dashboard.button("f", "󰈞  > Find file", ":Telescope find_files<CR>"),
+      dashboard.button("p", "  > Find project", ":lua require'telescope'.extensions.projects.projects{}<CR>"),
       dashboard.button("r", "  > Recent", ":Telescope oldfiles<CR>"),
       dashboard.button("s", "  > Settings", ":e ~/.config/nvim/lua/Libo/options.lua | :cd %:p:h | pwd<CR>"),
       dashboard.button("q", "󰩈  > Quit NVIM", ":qa<CR>"),
